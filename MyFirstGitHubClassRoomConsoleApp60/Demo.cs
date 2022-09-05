c2f9d029ebdcdfaacddf818a4d348474c9855142
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-09-05 12:43:18 +0200

    Created Demo Class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public int Demo1 { get; set; }
+        public int Demo2 { get; set; }
+
+        public Demo(int demo1, int demo2)
+        {
+            Demo1 = demo1;
+            Demo2 = demo2;
+        }
+
+        public override string ToString()
+        {
+            return $"Demo1 = {Demo1}; Demo2 = {Demo2}";
+        }
+    }
+}
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyFirstGitHubClassRoomConsoleApp60;
+
 Console.WriteLine("Hello, World!");
+var d = new Demo(1, 2);
+
+Console.WriteLine(d);
